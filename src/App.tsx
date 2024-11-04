668562d86c1ecb7bac55ab85437edfcd14cf2283
tinjet <tinjet1016@gmail.com>
2024-11-04 16:36:08 +0800

    feat: adding new route, toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
 import "./App.css";
-import NavBar from "./components/navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/administration";
-import NotFound from "./pages/not-found";
+import NotFound from "./pages/errorPage/NotFound";
+import { StaffRegisterForm } from "./pages/staffRegistration";
+import { StaffLoginForm } from "./pages/login/LoginForm";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+import { Toaster } from "react-hot-toast";
+import AdminProtectedRoute from "./components/AdminProtectedRoute";
+import PasswordRecovery from "./pages/passwordRecovery/PasswordRecoveryForm";
+import Unauthorized from "./pages/errorPage/Unauthorized";
+import Navbar from "./components/Navbar";
+import Forbidden from "./pages/errorPage/Forbidden";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <Toaster />
+    <Navbar /> 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Home />} />
-        <Route path="/about" element={<Home />} />
+        {/* Protected Routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+          //   <AdminProtectedRoute>
+            <StaffRegisterForm />
+            //</AdminProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Public Routes */}
+        <Route path="/login" element={<StaffLoginForm />} />
+        <Route path="/recover-password" element={<PasswordRecovery />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+        <Route path="/forbidden" element={<Forbidden/>}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
